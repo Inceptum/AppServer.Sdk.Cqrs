c81150b4417288993c4efc1b7c82ec64f4293906
Mikhail Tripolitov <tripolitov@gmail.com>
2019-03-26 17:30:45 +0300

    Move files from nested folder and add Tests project

---
+++ src/Inceptum.AppServer.Sdk.Cqrs.Tests/InvestigationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Inceptum.AppServer.Sdk.Cqrs.Tests
+{
+    [TestFixture]
+    internal class InvestigationTests
+    {
+        [Test]
+        public void AlwaysGreen()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/Inceptum.AppServer.Sdk.Cqrs.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Inceptum.AppServer.Sdk.Cqrs.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ce14103f-bd63-4f87-a68f-e81b594a4b70")]
