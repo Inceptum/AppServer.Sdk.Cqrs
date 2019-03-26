af1037689d4b900a0e46b568d3dfc2752faa5874
Mikhail Tripolitov <tripolitov@gmail.com>
2019-03-26 13:04:01 +0300

    Move code from Inceptum.AppServer package

---
+++ src/Inceptum.AppServer.Sdk.Cqrs/Inceptum.AppServer.Sdk.Cqrs/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Inceptum.AppServer.Sdk.Cqrs")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bbc2225c-4ed1-4072-a91e-b556f15e927a")]
---
+++ src/SolutionInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+
+[assembly: AssemblyCompany("InceptumSoft")]
+[assembly: AssemblyProduct("Inceptum.AppServer")]
+[assembly: AssemblyCopyright("Copyright © InceptumSoft 2013")]
+[assembly: AssemblyTrademark("Inceptum.AppServer")]
+[assembly: AssemblyCulture("")]
